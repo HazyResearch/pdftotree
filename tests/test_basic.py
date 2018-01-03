0dfc5105386e7856b21d81db7851b9eb38fd4068
Luke Hsiao <lwhsiao@stanford.edu>
2018-01-02 23:09:35 -0800

    Switch to paleo example

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,7 +3,7 @@ from tests.context import pdftotree
 
 def test_completion():
     """Simply test that parse runs to completion without errors."""
-    output = pdftotree.parse("tests/input/112823.pdf", "tests/output/", debug=True)
+    output = pdftotree.parse("tests/input/paleo.pdf", "tests/output/", debug=True)
 
     assert output is not None
 
