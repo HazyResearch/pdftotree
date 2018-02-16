1c84abaf6cf0c89908e2a4b697f5d556caaf7a65
Luke Hsiao <lwhsiao@stanford.edu>
2018-02-15 16:38:14 -0800

    Tweaks

--- pdftotree/__init__.py
+++ pdftotree/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-
 from pdftotree._version import __version__
 
 # At the top level, prevent logging output in absense of logging config.
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
+from pdftotree import tesseract
 from pdftotree.core import parse
