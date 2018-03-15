acbec4da201a1ca2cba50ea5d2a07000300ff891
ines-chami <ineschami@hotmail.com>
2018-03-15 15:50:44 -0700

    added visual test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -9,9 +9,14 @@ def test_heuristic_completion():
     output = pdftotree.parse("tests/input/paleo.pdf")
     assert output is not None
 
-
 def test_ml_completion():
     """Simply test that ML-based parse runs without errors."""
     output = pdftotree.parse(
-        "tests/input/paleo.pdf", model_path="tests/input/paleo_model.pkl")
+        "tests/input/paleo.pdf", model_type="ml", model_path="tests/input/paleo_model.pkl")
+    assert output is not None
+
+def test_visual_completion():
+    """Simply test that ML-based parse runs without errors."""
+    output = pdftotree.parse(
+        "tests/input/paleo.pdf", model_type="visual", model_path="tests/input/paleo_model.h5")
     assert output is not None
