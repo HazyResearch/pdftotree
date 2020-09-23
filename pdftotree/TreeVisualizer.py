2b38857649bb09f97765ed49122de732d1aaef90
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2020-09-23 15:04:49 -0700

    Use pdfminer instead of Poppler's pdftotext for page size

--- pdftotree/TreeVisualizer.py
+++ pdftotree/TreeVisualizer.py
@@ -1,6 +1,8 @@
-import subprocess
+from typing import Tuple
 
-from bs4 import BeautifulSoup
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
 
 try:
     from IPython import get_ipython
@@ -93,14 +95,11 @@ class TreeVisualizer:
         return img
 
 
-def get_pdf_dim(pdf_file):
-    html_content = subprocess.check_output(
-        "pdftotext -f {} -l {} -bbox '{}' -".format("1", "1", pdf_file), shell=True
-    )
-    soup = BeautifulSoup(html_content, "html.parser")
-    pages = soup.find_all("page")
-    page_width, page_height = (
-        int(float(pages[0].get("width"))),
-        int(float(pages[0].get("height"))),
-    )
+def get_pdf_dim(pdf_file) -> Tuple[int, int]:
+    with open(pdf_file, "rb") as f:
+        parser = PDFParser(f)
+        doc = PDFDocument(parser)
+        # Look at the 1st page only.
+        page = next(PDFPage.create_pages(doc))
+        _, _, page_width, page_height = tuple(page.mediabox)
     return page_width, page_height
